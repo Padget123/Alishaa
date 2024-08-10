146f9b4d92e435285816cec311361470e97432e8
Mohammed Rafiq H <120514001+Padget123@users.noreply.github.com>
2024-08-10 14:52:48 +0530

    Update Youtube.py

--- SYSTUM/platforms/Youtube.py
+++ SYSTUM/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from BRANDEDKING.utils.database import is_on_off
-from BRANDEDKING.utils.formatters import time_to_seconds
+from SYSTUM.utils.database import is_on_off
+from SYSTUM.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
